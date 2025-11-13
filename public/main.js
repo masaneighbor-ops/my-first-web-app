7d6786b16f9d93875309d3cc472ec6c497dd3826
masaneighbor-ops <masaneighbor6161@gmail.com>
2025-11-13 20:11:22 +0900

    Delete public directory

--- public/main.js
+++
@@ -1,5 +0,0 @@
-// ここからコードを書いてください
-import { setupTabs } from "./js/tabs.js";
-document.addEventListener("DOMContentLoaded", setupTabs);
-import { setupConverter } from "./js/converter.js";
-document.addEventListener("DOMContentLoaded", setupConverter);
